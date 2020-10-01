93373c54b19f0c52afefc5ac571cf97a33abe815
Daniel Scardigno <71049393+danscar3@users.noreply.github.com>
2020-10-01 14:34:01 -0400

    Avg code with loop

---
+++ avg.py
@@ -0,0 +1,23 @@
+numberofScores = 0
+score1 = 0
+score2 = 0 
+total = 0
+average = 0.0
+scoreCount = 0 
+#numberOfScores= int(input("please enter the number of scores you want to average: ")
+
+#What python loop strcuture could we use to repeat the new 3 lines
+    score1 = int (input("Please enter a test score: "))
+    score2 = int (input("Please enter a test score: "))
+    total = total + score 
+    scoreCount = scoreCount + 1
+
+
+    average = (Total) / number of scores
+    print(average)
+
+#simple counting loop with range
+print ("simple counting loop with range")
+for x in range(0,10):
+    print ("x= ",x)
+print()    
